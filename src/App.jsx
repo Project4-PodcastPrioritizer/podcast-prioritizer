9c79496f364c4a0ea2aa2c5a3adaa4fcb101a923
MicheleStaffiere <michelestaffiere1@gmail.com>
2023-09-15 03:46:58 -0400

    Prediciton api intergrated

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,7 @@ import Geolocation from './components/Geolocation.jsx'
 
 function App() {
   const [selectedPodcast,setSelectedPodcast] = useState([]);
-  const [podcastLength, setPodcaastLength] = useState("")
-
-  useEffect(()=>{
-    console.log("from app file " + selectedPodcast.podcast_title_original);
-    console.log("from app file " + podcastLength);
-  },[podcastLength,selectedPodcast])
+  const [podcastLength, setPodcaastLength] = useState("");
 
   return (
     <>
