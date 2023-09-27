850bb7bde98add6468f2992b3bf440777aa03685
Felipe Leung <fleung911@gmail.com>
2023-09-27 18:06:24 -0400

    Created AppBriefComponent

---
+++ src/components/AppBriefDescription.jsx
@@ -0,0 +1,17 @@
+// AppBriefDescription.js
+
+import React from 'react';
+
+function AppBriefDescription() {
+    return (
+        <AppBriefDescription>
+            <h2>Your ultimate podcast journey</h2>
+            <p>Welcome to Podcast Prioritizer, the cutting-edge podcast App that takes your listening experience to the next level. With Podcast Prioritizer, you'renot just a listerner; you're an adventurer on a podcast journey</p>
+            <p>Our innovative App combines the power of Listener API technology with the robust MapQuest API to create a unique and immersive podcasting experience.</p>
+            <p>Whether you're a seasoned podcast aficionado or just beginning your podcast journey, Podcast Prioritizer transforms your listening into an adventure</p>
+            <p>Explore, discover, and engage with the stories that matter to you, whrever you go.</p>
+        </AppBriefDescription>
+    );
+}
+
+export default AppBriefDescription;
